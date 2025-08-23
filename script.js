9b5ccf57e5d6dbb52ba482e95b4cd5591f537b45
khadeja8 <kadoja055@gmail.com>
2025-08-23 11:21:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// الحصول على كل العناصر
+const links = document.querySelectorAll('.navbar li');
 
+// إضافة حدث عند الضغط
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    // إزالة active من الكل
+    links.forEach(l => l.classList.remove('active'));
+    // إضافة active للعنصر الحالي
+    link.classList.add('active');
+  });
+});
